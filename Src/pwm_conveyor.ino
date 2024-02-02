264f2a5f938509047a1d9030a75444b1fd506ae8
devancakra <mudrawijaya@gmail.com>
2024-02-02 17:03:13 +0700

    Add: pwm_conveyor.ino

---
+++ Src/pwm_conveyor.ino
@@ -0,0 +1,49 @@
+// global variable initialization
+#define PB1_PIN 2 // PD2 pin to push button 1
+#define ENA_PIN 3 // PD3 pin to enable motor A
+#define PB2_PIN 4 // PD4 pin to push button 2
+#define IN1_PIN 5 // PD5 pin to rotate the motor backward
+#define IN2_PIN 6 // PD6 pin to rotate the motor forward
+
+boolean state1 = false, state2 = false; // initialize the status of digital readings on push buttons 1 and 2 into boolean data type
+
+// Method: setup
+void setup() {
+  pinMode(PB1_PIN, INPUT_PULLUP);     // PD2 pin is used as an input
+  pinMode(ENA_PIN, OUTPUT);           // PD3 pin is used as an output
+  pinMode(PB2_PIN, INPUT_PULLUP);     // PD4 pin is used as an input
+  pinMode(IN1_PIN, OUTPUT);           // PD5 pin is used as an output
+  pinMode(IN2_PIN, OUTPUT);           // PD6 pin is used as an output
+}
+
+// Method: loop
+void loop() {
+  analogWrite(ENA_PIN, 100); // Static PWM
+  
+  forward(); // calling the forward method
+  reverse(); // calling the reverse method
+  
+  delay(1000); // delay -> 1 second
+}
+
+// Method: forward
+void forward(){
+  state1 = digitalRead(PB1_PIN); // digital readout of button 1
+  if(digitalRead(IN2_PIN)==LOW && state1==LOW){ // if button 1 is pressed when the motor is off then:
+    digitalWrite(IN1_PIN, LOW); digitalWrite(IN2_PIN, HIGH); state1 = true; // motor rotation will be clockwise
+  }
+  else if(digitalRead(IN2_PIN)==HIGH && state1==LOW){ // if button 1 is pressed while the motor is running then :
+    digitalWrite(IN1_PIN, LOW); digitalWrite(IN2_PIN, LOW); state1 = true; // the motor will be shut down as soon as possible
+  }
+}
+
+// Method: reverse
+void reverse(){
+  state2 = digitalRead(PB2_PIN); // digital readout of button 2
+  if (digitalRead(IN1_PIN)==LOW && state2==LOW) { // if button 2 is pressed when the motor is off then:
+    digitalWrite(IN1_PIN, HIGH); digitalWrite(IN2_PIN, LOW); state2 = true; // motor rotation will be counterclockwise
+  }
+  else if(digitalRead(IN1_PIN)==HIGH && state2==LOW){ // if button 2 is pressed while the motor is running then :
+    digitalWrite(IN1_PIN, LOW); digitalWrite(IN2_PIN, LOW); state2 = true; // the motor will be shut down as soon as possible
+  }
+}
